3dd1a664b5cbefcd02c913d280c079eda95fc0b9
Ximin Luo <infinity0@pwned.gg>
2021-04-18 14:07:14 +0100

    automatically add all files relevant to a patch so devs don't have to

--- strict-containers/edit-patch.sh
+++ strict-containers/edit-patch.sh
@@ -1,15 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # Reverses a patch then applies it using quilt(1) for easy refreshing later.
 #
 # Run this on a given patch, then edit the working tree as normal.
-#
-# To add files to a patch being edited, run "quilt add <files>". You must do
-# this *before* editing the file, or else `./freeze-patch.sh` won't work later.
 #
 # When you are done, run `./freeze-patch.sh` (no args required) to update the
 # patch file with your working tree changes.
 #
 set -e
+shopt -s globstar
 
 which quilt >/dev/null 2>&1 || { echo >&2 "you need to install quilt(1)"; exit 1; }
 test -n "$1" || { echo >&2 "Usage: $0 <patch basename>"; exit 2; }
@@ -20,4 +18,23 @@ echo "Reversing patch $patch"
 patch -s -r- -p1 -R < "patches/$patch"
 echo "$patch" > patches/series
 
+quilt_() {
+  quilt "$@" && local x=$? || local x=$?
+  case $x in
+   2 ) return 0;; # quilt is a special snowflake that ignores unix exit code convention
+   * ) return $x;;
+  esac
+}
+
 quilt --quiltrc=.quiltrc push -q "$patch"
+
+if [ "${patch%.patch}" = "tests" ]; then
+  quilt_ add tests/**/*.hs
+else
+  if [ -f "src/Data/Strict/${patch%.patch}.hs" ]; then
+    quilt_ add "src/Data/Strict/${patch%.patch}.hs"
+  fi
+  if [ -d "src/Data/Strict/${patch%.patch}" ]; then
+    quilt_ add "src/Data/Strict/${patch%.patch}"/**/*.hs
+  fi
+fi
